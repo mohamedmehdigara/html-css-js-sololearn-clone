2b32b0d6937c690ed0ab9c9a536d2720aef03567
mohamedmehdigara <garamehdi57@yahoo.com>
2024-06-09 23:46:06 +0100

    edited files

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ lessonTitle.addEventListener('click', () => {
 });
 
 // Add similar functionality for toggling code editor visibility (if desired)
+
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    navLinks.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
